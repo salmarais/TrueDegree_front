8443e98387ca443e999a885b3f2484a6bf7b3603
unknown <salmamrais@gmail.com>
2021-01-28 20:57:00 +0100

    created login page

---
+++ src/views/Login.js
@@ -0,0 +1,118 @@
+import React from "react";
+import {Col, Row, Card, CardBody, CardHeader, CardTitle, FormGroup, Input, Button} from "reactstrap";
+import { LOGIN } from "variables/api";
+
+class Login extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: null,
+            message: null,
+            token: null,
+            email: null,
+            password: null
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    // handleInputChange(event) {
+    //     console.log('hangle input')
+    //     this.setState({
+    //         cin: event.target.value
+    //     });
+    // }
+
+
+    handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit = data => {
+        console.log(this.state);
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: this.state.email, password: this.state.password })
+        };
+        fetch(LOGIN, requestOptions)
+            .then(res => {
+                res.json();
+                console.log(res.headers)
+            })
+            .then(data => {
+                this.setState({ status: data.status, message: data.message })
+                
+            } )
+            .catch(console.log);        
+
+        data.preventDefault();
+    }
+
+    render() {
+        return(
+            <>
+                <div className="content">
+                    <Row>
+                        <Col md="8">
+                            <Card className="wrapping-center content-center align-content-md-center">
+                                <CardHeader>
+                                    <CardTitle>Login</CardTitle>
+                                </CardHeader>
+                                <CardBody>
+                                    <Row>
+                                    <Col md="12" sm="12">
+                                            <FormGroup>
+                                                <label>Email</label>
+                                                <Input
+                                                    defaultValue=""
+                                                    placeholder="Email"
+                                                    type="email"
+                                                    onChange = {this.handleInputChange}
+                                                />
+                                            </FormGroup>
+                                        </Col><Col md="12" sm="12">
+                                            <FormGroup>
+                                                <label>Password</label>
+                                                <Input
+                                                    defaultValue=""
+                                                    placeholder="Password"
+                                                    type="password"
+                                                    onChange = {this.handleInputChange}
+                                                />
+                                            </FormGroup>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <div className="ml-auto mr-auto">
+                                                <Button
+                                                    className="btn-primary"
+                                                    color="primary"
+                                                    type="submit"
+                                                    onClick = {this.handleSubmit}
+                                                >
+                                                    Search
+                                                </Button>
+                                            </div>
+                                        </Col>
+                                    </Row>
+                                </CardBody>
+                            </Card>
+                        </Col>
+                    </Row>
+                </div>
+            </>
+        );
+    }
+}
+
+export default Login;
